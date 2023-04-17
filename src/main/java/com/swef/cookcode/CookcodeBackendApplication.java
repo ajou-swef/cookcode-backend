73e320b96065869138bbd9f97e65dbf1d6297127
julie0005 <julie0005@ajou.ac.kr>
2023-04-17 20:01:23 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/com/swef/cookcode/CookcodeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swef.cookcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookcodeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookcodeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swef/cookcode/CookcodeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swef.cookcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookcodeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
