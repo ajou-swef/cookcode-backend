3b3ceb49ec9ae7c8aa20444c4f44194373225f7c
julie0005 <julie0005@ajou.ac.kr>
2023-04-19 18:19:43 +0900

    feat : 구독 연관관계 테이블 작성

---
+++ src/main/java/com/swef/cookcode/user/domain/Subscribe.java
@@ -0,0 +1,44 @@
+package com.swef.cookcode.user.domain;
+
+import com.swef.cookcode.recipe.domain.Recipe;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+        name = "subscribe",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "unq_subscribe_user_id",
+                        columnNames = {"subscriber_id", "publisher_id"}
+                )
+        }
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Subscribe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subscribe_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subscriber_id")
+    private User subscriber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "publisher_id")
+    private User publisher;
+}
