f6ce88d5d24e82585ca18c8d0b5c3348b986a5fb
skck0226 <skck0226@ajou.ac.kr>
2023-04-19 14:25:28 +0900

    feat : cookie 엔티티 정의 (연관관계 적용 이전)

--- src/main/java/com/swef/cookcode/cookie/domain/Cookie.java
+++ src/main/java/com/swef/cookcode/cookie/domain/Cookie.java
@@ -1,4 +1,29 @@
 package com.swef.cookcode.cookie.domain;
 
-public class Cookie {
+import com.swef.cookcode.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cookie")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Cookie extends BaseEntity {
+
+    @Id
+    @Column(name = "cookie_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cookieId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "video_url")
+    private String videoUrl;
+
 }
