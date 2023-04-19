b29fc532e6d6f1b5e3d167c4658b3816daade220
skck0226 <skck0226@ajou.ac.kr>
2023-04-19 14:51:22 +0900

    feat : fridge 엔티티 정의 (연관관계 적용 이전)

--- src/main/java/com/swef/cookcode/fridge/domain/Fridge.java
+++ src/main/java/com/swef/cookcode/fridge/domain/Fridge.java
@@ -1,4 +1,23 @@
 package com.swef.cookcode.fridge.domain;
 
-public class Fridge {
+import com.swef.cookcode.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "fridge")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Fridge extends BaseEntity {
+
+    @Id
+    @Column(name = "fridge_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int fridgeId;
+
 }
