c07f93b9ea3512b28f1abafcb4c269811382e308
julie0005 <julie0005@ajou.ac.kr>
2023-05-02 10:00:06 +0900

    fix : 출력 처리와 백그라운드 실행 처리

--- deploy.sh
+++ deploy.sh
@@ -19,4 +19,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포"
-nohup java -Dspring.profiles.active=dev -jar $JAR_PATH &
+nohup java -Dspring.profiles.active=dev -jar $JAR_PATH > /output/stdout.txt 2> /output/stderr.txt < /output/stdin.txt &
