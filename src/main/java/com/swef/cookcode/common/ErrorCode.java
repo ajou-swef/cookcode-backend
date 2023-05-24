4cf525f93e5d721780e8fb0e06128a51dd46f95f
신기철 <skck0226@ajou.ac.kr>
2023-05-24 12:51:09 +0900

    Cookie 조회 구현 (#40)
    * feat: Cookie pagination 조회 구현
    
    * feat: 유저의 쿠키 조회 구현
    
    ---------
    
    Co-authored-by: julie0005 <julie0005@ajou.ac.kr>

--- src/main/java/com/swef/cookcode/common/ErrorCode.java
+++ src/main/java/com/swef/cookcode/common/ErrorCode.java
@@ -67,7 +67,14 @@ public enum ErrorCode {
   /*
   Recipe Domain
    */
-  RECIPE_NOT_FOUND(400, "I001", "존재하지 않는 레시피입니다.");
+  RECIPE_NOT_FOUND(400, "I001", "존재하지 않는 레시피입니다."),
+
+  /*
+  Cookie Domain
+   */
+  COOKIE_NOT_FOUND(400, "K001", "존재하지 않는 쿠키입니다.");
+
+
 
   private final int status;
   private final String code;
--- src/main/java/com/swef/cookcode/cookie/service/CookieService.java
+++ src/main/java/com/swef/cookcode/cookie/service/CookieService.java
@@ -8,18 +8,39 @@ import com.swef.cookcode.recipe.domain.Recipe;
 import com.swef.cookcode.recipe.repository.RecipeRepository;
 import com.swef.cookcode.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Pageable;
+import com.swef.cookcode.common.error.exception.NotFoundException;
+import com.swef.cookcode.common.ErrorCode;
 
 @Service
 @RequiredArgsConstructor
 public class CookieService {
 
-    private final S3Util s3Util;
-
     private final CookieRepository cookieRepository;
 
+    private final S3Util s3Util;
+
     private final RecipeRepository recipeRepository;
+
+
+    @Transactional
+    public Slice<Cookie> getRandomCookies(Pageable pageable) {
+        return cookieRepository.findRandomCookies(pageable);
+    }
+
+    @Transactional
+    public Cookie getCookieById(Long cookieId) {
+        return cookieRepository.findById(cookieId)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.COOKIE_NOT_FOUND));
+    }
+
+    @Transactional
+    public Slice<Cookie> getCookiesOfUser(Pageable pageable, Long userId) {
+        return cookieRepository.findByUserId(pageable, userId);
+    }
 
     @Transactional
     public void createCookie(User user, CookieCreateRequest request){
