2beecf3def03a521951630e99e1fd8b29fdaecdb
신기철 <skck0226@ajou.ac.kr>
2023-06-04 16:58:56 +0900

    쿠키 썸네일 추출 구현 (#81)
    * feat: InputStream에서 썸네일 추출 유틸 메서드 구현
    
    * feat: Cookie 엔티티 dto ThumbnailUrl 필드 추가
    
    * feat: 쿠키 썸네일 추출 후 업로드 구현

--- src/main/java/com/swef/cookcode/common/error/exception/ThumbnailException.java
+++ src/main/java/com/swef/cookcode/common/error/exception/ThumbnailException.java
@@ -4,11 +4,10 @@ import com.swef.cookcode.common.ErrorCode;
 import lombok.Getter;
 
 @Getter
-public class S3Exception  extends RuntimeException{
-
+public class ThumbnailException extends RuntimeException{
     private final ErrorCode errorCode;
 
-    public S3Exception(ErrorCode errorCode) {
+    public ThumbnailException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
