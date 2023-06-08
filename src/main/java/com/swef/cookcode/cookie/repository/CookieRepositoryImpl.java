e6c01f671dd158e715658a0424271295af808f5c
Binfoo <julie0005@ajou.ac.kr>
2023-06-08 20:52:35 +0900

    쿠키 페이지네이션 버그 수정 (#93)
    * fix : 유저의 쿠키 조회 시 페이지네이션 수정
    
    * refactor : cookieResponse isLiked boolean 으로 변경

--- src/main/java/com/swef/cookcode/cookie/repository/CookieRepositoryImpl.java
+++ src/main/java/com/swef/cookcode/cookie/repository/CookieRepositoryImpl.java
@@ -1,20 +1,21 @@
 package com.swef.cookcode.cookie.repository;
 
+import static com.swef.cookcode.cookie.domain.QCookie.cookie;
+import static com.swef.cookcode.cookie.domain.QCookieComment.cookieComment;
+import static com.swef.cookcode.cookie.domain.QCookieLike.cookieLike;
+import static com.swef.cookcode.recipe.domain.QRecipe.recipe;
+import static com.swef.cookcode.user.domain.QUser.user;
+
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import static com.swef.cookcode.cookie.domain.QCookie.cookie;
-import static com.swef.cookcode.cookie.domain.QCookieComment.cookieComment;
-import static com.swef.cookcode.cookie.domain.QCookieLike.cookieLike;
-import static com.swef.cookcode.user.domain.QUser.user;
-import static com.swef.cookcode.recipe.domain.QRecipe.recipe;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.swef.cookcode.common.Util;
-import java.util.List;
-
 import com.swef.cookcode.cookie.dto.CookieResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -29,8 +30,9 @@ public class CookieRepositoryImpl implements CookieCustomRepository{
     @Override
     public Slice<CookieResponse> searchCookies(String query, Long userId, Pageable pageable) {
         List<CookieResponse> responses = selectCookieResponseFromCookieUserJoinAndLikeComment(userId)
-                .fetchJoin()
                 .where(cookieSearchContains(query))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
                 .fetch();
 
         return new SliceImpl<>(responses, pageable, Util.hasNextInSlice(responses, pageable));
@@ -44,13 +46,12 @@ public class CookieRepositoryImpl implements CookieCustomRepository{
 
     @Override
     public Slice<CookieResponse> findByTargetUserId(Pageable pageable, Long targetUserId, Long userId) {
-        return new SliceImpl<>(
-            selectCookieResponseFromCookieUserJoinAndLikeComment(userId)
+        List<CookieResponse> responses = selectCookieResponseFromCookieUserJoinAndLikeComment(userId)
                 .where(user.id.eq(targetUserId))
-                .offset((long) pageable.getPageNumber() * pageable.getPageSize())
-                .limit(pageable.getPageSize())
-                .fetch()
-        );
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+        return new SliceImpl<>(responses, pageable, Util.hasNextInSlice(responses, pageable));
     }
 
     @Override
@@ -76,15 +77,21 @@ public class CookieRepositoryImpl implements CookieCustomRepository{
     private JPAQuery<CookieResponse> selectCookieResponseFromCookieUserJoinAndLikeComment(Long userId){
         return queryFactory.select(Projections.constructor(CookieResponse.class,
                         cookie,
-                        selectIsCookiLikedByUser(userId),
+                        isLikedExpression(userId),
                         selectCookieLikeCount(),
                         selectCookieCommentCount()))
                 .from(cookie)
-                .leftJoin(recipe).on(recipe.id.eq(cookie.recipe.id))
-                .join(cookie.user, user);
+                .join(cookie.user)
+                .fetchJoin()
+                .leftJoin(recipe).on(recipe.id.eq(cookie.recipe.id));
     }
 
-    private JPQLQuery<Long> selectIsCookiLikedByUser(Long userId){
+    private BooleanExpression isLikedExpression(Long userId) {
+        return new CaseBuilder()
+                .when(selectIsCookieLikedByUser(userId).eq(0L)).then(false).otherwise(true);
+    }
+
+    private JPQLQuery<Long> selectIsCookieLikedByUser(Long userId){
         return JPAExpressions.select(cookieLike.count())
                 .from(cookieLike)
                 .where(cookieLike.cookie.id.eq(cookie.id)
