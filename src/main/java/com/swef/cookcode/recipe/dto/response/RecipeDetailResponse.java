13696a3d87f521770081845ce2bee075c82e060b
julie0005 <julie0005@ajou.ac.kr>
2023-06-12 00:11:16 +0900

    fix : 레시피 상세 조회 dto 수정

--- src/main/java/com/swef/cookcode/recipe/dto/response/RecipeDetailResponse.java
+++ src/main/java/com/swef/cookcode/recipe/dto/response/RecipeDetailResponse.java
@@ -21,8 +21,8 @@ public class RecipeDetailResponse extends RecipeResponse{
     private List<IngredSimpleResponse> optionalIngredients;
 
 
-    public RecipeDetailResponse(Recipe recipe, Boolean isCookable, Long likeCount, Boolean isLiked, Long commentCount) {
-        super(recipe, isCookable, likeCount, isLiked, commentCount, true);
+    public RecipeDetailResponse(Recipe recipe, Boolean isCookable, Long likeCount, Boolean isLiked, Long commentCount, Boolean isAccessible) {
+        super(recipe, isCookable, likeCount, isLiked, commentCount, isAccessible);
         this.steps = recipe.getSteps().stream().map(s -> StepResponse.from(s, s.getPhotos(), s.getVideos())).toList();
     }
 
