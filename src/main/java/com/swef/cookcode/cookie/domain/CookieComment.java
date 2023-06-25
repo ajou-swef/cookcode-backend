d2fde711672ee97ce80b91f697ddfb1e852b3dbf
julie0005 <julie0005@ajou.ac.kr>
2023-06-25 18:25:55 +0900

    refactor : comment 공통 부분 mappedSuperClass 통해 추출

--- src/main/java/com/swef/cookcode/cookie/domain/CookieComment.java
+++ src/main/java/com/swef/cookcode/cookie/domain/CookieComment.java
@@ -1,8 +1,12 @@
 package com.swef.cookcode.cookie.domain;
 
-import com.swef.cookcode.common.entity.BaseEntity;
+import com.swef.cookcode.common.entity.Comment;
 import com.swef.cookcode.user.domain.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,30 +15,15 @@ import lombok.NoArgsConstructor;
 @Table(name = "cookie_comment")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class CookieComment extends BaseEntity {
-
-    private static final int MAX_COMMENT_LENGTH = 300;
-
-    @Id
-    @Column(name = "cookie_comment_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+public class CookieComment extends Comment {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cookie_id")
     private Cookie cookie;
 
-    @Column(name = "comment", nullable = false, length = MAX_COMMENT_LENGTH)
-    private String comment;
-
     public CookieComment(User user, Cookie cookie, String comment) {
-        this.user = user;
+        super(user, comment);
         this.cookie = cookie;
-        this.comment = comment;
     }
 
     public static CookieComment createEntity(User user, Cookie cookie, String comment) {
