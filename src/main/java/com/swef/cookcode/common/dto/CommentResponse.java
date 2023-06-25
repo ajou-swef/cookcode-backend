71e2161f5a711d5d1be248deb8f287fec51cc479
julie0005 <julie0005@ajou.ac.kr>
2023-06-25 18:51:21 +0900

    refactor : dto from 메소드 통일

--- src/main/java/com/swef/cookcode/common/dto/CommentResponse.java
+++ src/main/java/com/swef/cookcode/common/dto/CommentResponse.java
@@ -1,7 +1,6 @@
 package com.swef.cookcode.common.dto;
 
-import com.swef.cookcode.cookie.domain.CookieComment;
-import com.swef.cookcode.recipe.domain.RecipeComment;
+import com.swef.cookcode.common.entity.Comment;
 import com.swef.cookcode.user.dto.response.UserSimpleResponse;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
@@ -15,15 +14,7 @@ public class CommentResponse {
 
     private final String comment;
 
-    public static CommentResponse from(RecipeComment comment) {
-        return CommentResponse.builder()
-                .commentId(comment.getId())
-                .user(UserSimpleResponse.from(comment.getUser()))
-                .comment(comment.getComment())
-                .build();
-    }
-
-    public static CommentResponse from(CookieComment comment) {
+    public static CommentResponse from(Comment comment) {
         return CommentResponse.builder()
                 .commentId(comment.getId())
                 .user(UserSimpleResponse.from(comment.getUser()))
